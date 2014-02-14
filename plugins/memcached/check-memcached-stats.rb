424a1d8c97363a5cd1d243b7e56a712b40c26129
b3limawr <ryan.will.be@gmail.com>
2014-02-14 14:51:41 -0800

    Added check for percent of configured memory used
    A critical error will be triggered when the memory being used by memcached passes the threshold of the total memory limit. A special thank to jeffWelling for helping with the ruby code.

--- plugins/memcached/check-memcached-stats.rb
+++ plugins/memcached/check-memcached-stats.rb
@@ -28,20 +28,40 @@ class MemcachedStats < Sensu::Plugin::Check::CLI
          :description => "Memcached Port to connect to",
          :proc        => proc { |p| p.to_i },
          :default     => 11211
-
+  option :critical,
+         :short       => "-c PCT",
+         :long        => "--critical THRESHOLD PERCENT",
+         :description => "critical threshold percent",
+         :proc        => proc { |p| p.to_i },
+         :default     => 90
   def run
     begin
-      Timeout.timeout(30) do
+      Timeout.timeout(29) do
         TCPSocket.open(config[:host], config[:port]) do |socket|
           socket.print "stats\r\n"
-          socket.close_write
-          socket.read
+	  socket.close_write
+          output = socket.read
+          output2=Hash.new
+          output.split("\r").each {|line|
+            line.match('STAT'){|stat|
+	      output2.merge!( {line.split(' ')[1].to_sym => line.split(' ')[2]} )
+            }
+
+          }
+	 used_pct = ((output2[:bytes].to_f / output2[:limit_maxbytes].to_f) * 100).round(2)
+	  if used_pct >= config[:critical] 
+	    exit
+	  end
         end
       end
     rescue Timeout::Error
       warning "timed out connecting to memcached on port #{config[:port]}"
-    rescue
-      critical "Can't connect to port #{config[:port]}"
+    rescue Errno::ETIMEDOUT
+      warning "timed out connecting to memcached on port #{config[:port]}"
+    rescue Errno::ECONNREFUSED
+      critical "Can't connect to port #{config[:port]}" 
+    rescue Exception => e
+      critical "memcached has over #{config[:critical]}% memory usage"
     else
       ok "memcached stats protocol responded in a timely fashion"
     end
